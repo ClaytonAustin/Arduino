1af9ce216f7046e4956395bb4c0299f3deda5b31
ClaytonAustin <austinclaytonbuisness@gmail.com>
2022-03-03 08:01:17 -0800

    Dual Pin stability (11,10)

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -13,20 +13,25 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
 */
 
-int led = 11;           // the PWM pin the LED is attached to
+int ledElleven = 11;           // the PWM pin the LED is attached to
+int ledTen = 10;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 11 to be an output:
-  pinMode(led, OUTPUT);
+  pinMode(ledElleven, OUTPUT);
+  // declare pin 10 to be an output:
+  pinMode(ledTen, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 11:
-  analogWrite(led, brightness);
+  analogWrite(ledElleven, brightness);
+  // set the brightness of pin 10:
+  analogWrite(ledTen, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
